62f3ba7b53454bd8252686166508bfc39510476f
Miquel Abellán <miquelabellan@MBP-Miquel.local>
2017-03-31 12:00:20 +0200

    solucio d'errors

--- app/src/main/java/edu/lasalle/pprog2/practicafinal/MainActivity.java
+++ app/src/main/java/edu/lasalle/pprog2/practicafinal/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.lasalle.pprog2.practicafinal;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void enterActivity(View view) {
+
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivityForResult(intent, 2);
+
     }
 }
