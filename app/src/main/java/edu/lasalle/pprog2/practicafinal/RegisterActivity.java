ccf8dd273fd0822bf89cb7e198872d6c7fa8ae59
Miquel Abellán <miquelabellan@MBP-Miquel.local>
2017-04-03 18:29:56 +0200

    pas a vistes

--- app/src/main/java/edu/lasalle/pprog2/practicafinal/RegisterActivity.java
+++ app/src/main/java/edu/lasalle/pprog2/practicafinal/RegisterActivity.java
@@ -1,7 +1,12 @@
 package edu.lasalle.pprog2.practicafinal;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by miquelabellan on 31/3/17.
@@ -13,4 +18,33 @@ public class RegisterActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registro);
     }
+
+    public void takeAPictureOnClick (View view) {
+        // Creamos un intent implícito que llame a alguna aplicación capaz de tomar fotos.
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Lanzamos el intent para que nos devuelva un resultado y configuramos el requestCode
+        // para poder reconocer el valor de retorno.
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Filtramos los resultados de las distintas actividades según el requestCode.
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    Bundle bundle = data.getExtras();
+                    Bitmap image = (Bitmap) bundle.get("data");
+
+                    ImageView imageView = (ImageView) findViewById(R.id.imageView2);
+                    if (imageView != null) {
+                        imageView.setImageBitmap(image);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
 }
--- app/src/main/java/edu/lasalle/pprog2/practicafinal/Results.java
+++ app/src/main/java/edu/lasalle/pprog2/practicafinal/Results.java
@@ -1,33 +1,22 @@
 package edu.lasalle.pprog2.practicafinal;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.View;
 
 /**
  * Created by miquelabellan on 31/3/17.
  */
 
-public class SearchActivity extends AppCompatActivity{
+public class Results extends AppCompatActivity {
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pantalla_busqueda);
+        setContentView(R.layout.results_layout);
     }
 
-    public void buscarPerNom(View view) {
-        Intent intent = new Intent(this, Results.class);
-        startActivityForResult(intent, 2);
-    }
-
-    public void buscaPerLocalitzacio(View view) {
-        Intent intent = new Intent(this, Results.class);
-        startActivityForResult(intent, 2);
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Este metodo se llamara una vez durante la creacion de la Activity
@@ -35,5 +24,6 @@ public class SearchActivity extends AppCompatActivity{
         inflater.inflate(R.menu.action_bar3, menu);
         return true;
     }
+
 
 }
