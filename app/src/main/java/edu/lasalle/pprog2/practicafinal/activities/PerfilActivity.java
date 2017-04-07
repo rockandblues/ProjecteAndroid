41c36632382bbfe349675d9061f6cb2a9d42acde
Miquel Abellán <miquelabellan@MBP-Miquel.local>
2017-04-07 19:47:47 +0200

    Camara editar perfil arreglada

--- app/src/main/java/edu/lasalle/pprog2/practicafinal/activities/PerfilActivity.java
+++ app/src/main/java/edu/lasalle/pprog2/practicafinal/activities/PerfilActivity.java
@@ -1,6 +1,9 @@
 package edu.lasalle.pprog2.practicafinal.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -8,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import edu.lasalle.pprog2.practicafinal.R;
 
@@ -59,6 +63,35 @@ public class PerfilActivity extends AppCompatActivity {
 
 
     }
+
+    public void takeAPictureOnClick (View view) {
+        // Creamos un intent implícito que llame a alguna aplicación capaz de tomar fotos.
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Lanzamos el intent para que nos devuelva un resultado y configuramos el requestCode
+        // para poder reconocer el valor de retorno.
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Filtramos los resultados de las distintas actividades según el requestCode.
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    Bundle bundle = data.getExtras();
+                    Bitmap image = (Bitmap) bundle.get("data");
+
+                    ImageView imageView = (ImageView) findViewById(R.id.imageView3);
+                    if (imageView != null) {
+                        imageView.setImageBitmap(image);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
     public void finishActivity(MenuItem menuItem) {
         finishActivity(R.layout.perfil_layout);
     }
