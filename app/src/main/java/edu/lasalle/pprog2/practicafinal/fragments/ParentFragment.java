0683d67fbfc558027db8725e1e78f649098e26f9
joanfito <joan.fito.2015@salleurl.edu>
2017-05-16 16:56:27 +0200

    spinner muestra los tipos

--- app/src/main/java/edu/lasalle/pprog2/practicafinal/fragments/ParentFragment.java
+++ app/src/main/java/edu/lasalle/pprog2/practicafinal/fragments/ParentFragment.java
@@ -19,18 +19,28 @@ public abstract class ParentFragment extends Fragment {
 
     public void loadSpinner(ArrayList<Place> place, ActionBar3Activity activity) {
         int max = place.size();
-        //Creamos el adapter
+
         ArrayList<String> types = new ArrayList<>();
 
+        //Buscamos los tipos encontrados y los guardamos
         for(int i = 0; i < max; i++) {
-            types.add(place.get(i).getType());
-        }
 
+            int typeMax = types.size();
+            boolean exists = false;
+            //Miramos si ya tenemos el tipo guardado, si no lo esta, lo guardamos
+            for(int j = 0; j < typeMax; j++) {
+                if(types.get(j).equals(place.get(i).getType())) exists = true;
+            }
+
+            if(!exists) types.add(place.get(i).getType());
+        }
 
         //TODO solo funciona bien el spinner la primera vez
 
         //Creamos el adaptador
-        ArrayAdapter spinner_adapter = new ArrayAdapter(getContext(), android.R.layout.simple_spinner_item, types);
+        ArrayAdapter spinner_adapter =
+                new ArrayAdapter(getContext(), android.R.layout.simple_spinner_item, types);
+
         //Añadimos el layout para el menú y se lo damos al spinner
         spinner_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
